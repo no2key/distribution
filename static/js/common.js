b8d64cafda692493825cb28e01c003e38822218e
shuangluo <imluoshuang@gmail.com>
2013-07-03 14:43:10 +0800

    svn checkout时不要--delete

--- static/js/common.js
+++ static/js/common.js
@@ -5,3 +5,9 @@
  * Time: 上午10:46
  * To change this template use File | Settings | File Templates.
  */
+
+function showAddAnotherPopup(href, name) {
+    var win = window.open(href, name, 'height=500,width=800,resizable=yes,scrollbars=yes');
+    win.focus();
+    return false;
+}
