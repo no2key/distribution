6257e64715ac302d6e4c924683ced6c96875c7ec
youngsterxyf <sas.198708@gmail.com>
2013-07-05 19:58:40 +0800

    update

--- curl_monitor/urls.py
+++ curl_monitor/urls.py
@@ -4,10 +4,10 @@ from django.conf.urls import patterns, url
 import views
 
 urlpatterns = patterns('',
-    url(r'monitor_list/$', views.get_monitor_list),
-    url(r'add_monitor/$', views.add_monitor),
-    url(r'del_monitor/$', views.del_monitor),
-    url(r'modify_monitor/$', views.modify_monitor),
-    url(r'view_log_list/$', views.get_log_list),
-    url(r'view_log/$', views.get_log_content),
+    url(r'^monitor_list/$', views.get_monitor_list),
+    url(r'^add_monitor/$', views.add_monitor),
+    url(r'^del_monitor/$', views.del_monitor),
+    url(r'^modify_monitor/$', views.modify_monitor),
+    url(r'^view_log_list/$', views.get_log_list),
+    url(r'^view_log/$', views.get_log_content),
 )
