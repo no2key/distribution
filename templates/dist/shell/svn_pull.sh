6d859e3a937862f71ef85b2a8471888eb3ba383f
shuangluo <imluoshuang@gmail.com>
2013-07-23 10:11:34 +0800

    bugfix: svn pull

--- templates/dist/shell/svn_pull.sh
+++ templates/dist/shell/svn_pull.sh
@@ -1,13 +1,11 @@
 
-mkdir /home/svnroot/code/{{svn_package_path}};
-cd /home/svnroot/code/{{svn_package_path}};
+cd /home/svnroot/code/;
 svn co {{prefix}}/code/{{svn_package_path}} --username={{username}} --password={{password}};
-mkdir /home/svnroot/config/{{svn_config_path}};
-cd /home/svnroot/config/{{svn_config_path}};
+cd /home/svnroot/config/;
 svn co {{prefix}}/config/{{svn_config_path}} --username={{username}} --password={{password}};
 chmod -R 777 /home/svnroot/code/{{svn_package_path}};
 chmod -R 777 /home/svnroot/config/{{svn_config_path}};
-mkdir /home/code_config_merge/tmp_{{date}}/; "
+mkdir /home/code_config_merge/tmp_{{date}}/;
 rsync -av --exclude=.svn /home/svnroot/code/{{svn_package_path}}/ /home/code_config_merge/tmp_{{date}}/;
 rsync -av --exclude=.svn /home/svnroot/config/{{svn_config_path}}/ /home/code_config_merge/tmp_{{date}}/;
 rsync -e 'ssh -p 36000 -l Administrator' -azv --exclude=.svn /home/code_config_merge/tmp_{{date}}/ 192.168.2.140:/cygdrive/e/Publish/{{execute_machine}}/;
