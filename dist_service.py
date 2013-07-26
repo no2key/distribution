f69871f507a7ecffc2166fb163cff50ad4b844b9
shuangluo <imluoshuang@gmail.com>
2013-07-26 15:27:00 +0800

    dist_service

--- dist_service.py
+++ dist_service.py
@@ -3,15 +3,16 @@
 import os, sys
 
 def main(args):
+    cur_dir = os.getcwd()
     if len(args) != 2:
         print "Usage: %s start|stop|restart" % args[0]
         exit(-1)
     method = args[1]
     if method == 'start':
         os.system("""
-cd /home/distribution
+cd %s
 gunicorn distribution.wsgi:application -b 127.0.0.1:8002&
-python manage.py celery worker > /dev/null 2>&1&""")
+python manage.py celery worker > /dev/null 2>&1&""" % cur_dir)
     elif method == 'stop':
         os.system("kill -9 `ps -ef | grep gunicorn | grep -v grep | awk '{if(NR==1) print $2}'`")
         os.system("kill -9 `ps -ef | grep celery | grep worker | grep -v grep | awk '{if(NR==1) print $2}'`")
@@ -20,9 +21,9 @@ python manage.py celery worker > /dev/null 2>&1&""")
         os.system("kill -9 `ps -ef | grep celery | grep worker | grep -v grep | awk '{if(NR==1) print $2}'`")
         os.system("sleep 5")
         os.system("""
-cd /home/distribution
+cd %s
 gunicorn distribution.wsgi:application -b 127.0.0.1:8002&
-python manage.py celery worker > /dev/null 2>&1& """)
+python manage.py celery worker > /dev/null 2>&1& """ % cur_dir)
     else:
         print "Usage: %s start|stop|restart" % args[0]
         exit(-1)
